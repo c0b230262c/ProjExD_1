fc6dccb8a7e63f3eb61960ec199f1cb047268082
big oiwa <c0b230262c@edu.teu.ac.jp>
2024-06-11 17:26:39 +0900

    名前変更2

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -26,15 +26,21 @@ def main():
         screen.blit(bg_img, [-x+3200, 0])
         screen.blit(bg_img2, [-x+4800, 0])
         key_lst = pg.key.get_pressed()
+        a = -1
+        b = 0
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip(0, -1)
+            a = 0
+            b = -1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip(0, +1)
+            a = 0
+            b = 1
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip(-1, 0)
+            a = -2
+            b = 0
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(+2, 0)
-        kk_rct.move_ip(-1, 0)
+            a = 2
+            b = 0
+        kk_rct.move_ip(a, b)
         screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1       
