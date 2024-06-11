6686a574ed9e3d9c5a61ce3ec2c47de35c3f81f2
big oiwa <c0b230262c@edu.teu.ac.jp>
2024-06-11 16:19:43 +0900

    背景間延び対策

--- pygame_image.py
+++ pygame_image.py
@@ -17,14 +17,16 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
+        x = tmr%800
+        screen.blit(bg_img, [-x, 0])
         kk_rct = kk_img.get_rect()
         kk_rct.center = 300, 200
         screen.blit(kk_img, kk_rct)
         pg.display.update()
-        tmr += 1        
+        tmr += 1       
+         
         clock.tick(200)
+    
 
 
 if __name__ == "__main__":
